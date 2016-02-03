c562da663ef1fe9b3560d8ca4b044a0c2592020f
valentinsvt <valentinsvt@hotmail.com>
2016-02-03 11:15:26 -0500

    Archivos vagrant

---
+++ vagrantFiles/ansible.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+sudo apt-get -y install python-pip
