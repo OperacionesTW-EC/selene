38992091aae222b2d38d79548402e8e107417b50
valentinsvt <valentinsvt@hotmail.com>
2016-05-18 10:06:24 -0500

    #0 - fixing migrations

--- backend/devices/serializers.py
+++ backend/devices/serializers.py
@@ -20,9 +20,13 @@ class DeviceSerializer(serializers.ModelSerializer):
     device_state_name = serializers.CharField(read_only=True)
     full_code = serializers.CharField(read_only=True)
     purchase_date = serializers.DateField(required=False)
-    default_device_state = DeviceState.objects.get_or_create(name='Disponible')[0]
-    device_state = serializers.ModelField(model_field=Device()._meta.get_field('device_state'), default=default_device_state)
+    try:
+        default_device_state = DeviceState.objects.get_or_create(name='Disponible')[0]
+        device_state = serializers.ModelField(model_field=Device()._meta.get_field('device_state'), default=default_device_state)
+    except Exception:
+        print 'aaa'
+
     class Meta:
         model = Device
         fields = ('id', 'device_type_name', 'full_code', 'device_brand_name', 'device_type', 'device_brand', 'asset',
-                  'ownership', 'serial_number', 'model', 'purchase_date', 'device_state', 'device_state_name')
+              'ownership', 'serial_number', 'model', 'purchase_date', 'device_state', 'device_state_name')
