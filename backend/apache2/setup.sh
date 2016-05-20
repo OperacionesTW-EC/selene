387775f633b0e8918a36dcbfeb16a35215e83589
valentinsvt <valentinsvt@hotmail.com>
2016-05-20 12:09:33 -0500

    #0 - with @sauljabin - testing ci with migrations #4

--- backend/apache2/setup.sh
+++ backend/apache2/setup.sh
@@ -3,4 +3,7 @@ rm -rf /home/selene/backend
 tar -xvf /home/selene/selene_backend.tar -C /home/selene
 rm -rf /home/selene/selene_backend.tar
 cd /home/selene/backend/apache2
+cd /home/selene/backend
+python manage.py migrate
+chown -R selene /home/selene/backend
 systemctl restart httpd
