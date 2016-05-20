15a3c790890b25ce0854ed97d9d09152d2359b37
valentinsvt <valentinsvt@hotmail.com>
2016-05-20 12:13:52 -0500

    #0 - with @sauljabin - testing ci with migrations #5

--- backend/apache2/setup.sh
+++ backend/apache2/setup.sh
@@ -1,4 +1,5 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+source /home/selene/.bash_profile
 rm -rf /home/selene/backend
 tar -xvf /home/selene/selene_backend.tar -C /home/selene
 rm -rf /home/selene/selene_backend.tar
