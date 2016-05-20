a0a134a8b3f9c2f76c592bd2c06cc9c430020b3d
valentinsvt <valentinsvt@hotmail.com>
2016-05-20 11:19:57 -0500

    #0 - with @sauljabin - testing ci #1

--- backend/apache2/setup.sh
+++ backend/apache2/setup.sh
@@ -1,5 +1,5 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-rm -rf /home/selene/backend
+sudo rm -rf /home/selene/backend
 tar -xvf /home/selene/selene_backend.tar -C /home/selene
 cd /home/selene/backend/apache2
 sudo systemctl restart httpd
