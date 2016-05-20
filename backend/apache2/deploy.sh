8e1a55ba1375bbfc0206bcb2793f97215ea85f9d
valentinsvt <valentinsvt@hotmail.com>
2016-05-20 11:26:45 -0500

    #0 - with @sauljabin - testing ci #2

--- backend/apache2/deploy.sh
+++ backend/apache2/deploy.sh
@@ -1,7 +1,7 @@
 tput setaf 1;
 echo '--------------reiniciando el servidor------------------------------'
 tput sgr0;
-sudo systemctl restart httpd
+systemctl restart httpd
 tput setaf 1;
 echo '--------------Listo------------------------------'
 tput sgr0;
--- backend/apache2/setup.sh
+++ backend/apache2/setup.sh
@@ -1,6 +1,7 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-sudo rm -rf /home/selene/backend
+rm -rf /home/selene/backend
 tar -xvf /home/selene/selene_backend.tar -C /home/selene
+rm -rf /home/selene/selene_backend.tar
 cd /home/selene/backend/apache2
 sudo systemctl restart httpd
 
