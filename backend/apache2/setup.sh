90140467d8bb659622702415b3e6fc9e367e8814
valentinsvt <valentinsvt@hotmail.com>
2016-05-23 09:36:46 -0500

    #0 - fixing setup script

--- backend/apache2/setup.sh
+++ backend/apache2/setup.sh
@@ -3,7 +3,6 @@ source /home/selene/.bash_profile
 rm -rf /home/selene/backend
 tar -xvf /home/selene/selene_backend.tar -C /home/selene
 rm -rf /home/selene/selene_backend.tar
-cd /home/selene/backend/apache2
 cd /home/selene/backend
 python manage.py migrate
 chown -R selene /home/selene/backend
