ced6ad5c64f46540e936a588b339be0ea8d3b869
twcamper <twcamper@thoughtworks.com>
2016-06-02 16:30:35 -0500

    #25 - ensure python3, especially for the deploy

--- backend/apache2/setup.sh
+++ backend/apache2/setup.sh
@@ -6,6 +6,6 @@ rm -rf /home/selene/selene_backend.tar
 cd /home/selene/backend
 export DEVICE_FILE_PATH=/home/selene/migrationFiles/devices.csv
 pip3 install -r requirements.txt
-yes | python manage.py migrate
+yes | manage.py migrate
 chown -R selene:selene /home/selene/backend
 systemctl restart httpd
