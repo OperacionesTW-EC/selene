135035c97733d913a1a876c45b6f3730e685eb7e
twcamper <twcamper@thoughtworks.com>
2016-06-09 12:48:26 -0500

    add e2e-requirements.txt for py2; add e2e README; file formatting

--- backend/e2e/features/step/Steps.py
+++ backend/e2e/features/step/Steps.py
@@ -17,14 +17,16 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
 
+
 def contains_content(browser, content):
     for elem in browser.find_elements_by_xpath('//*[text()]'):
         # hypothetically it should be possible to make this request using
         # a contains() predicate, but that doesn't seem to behave properly
         if elem.is_displayed() and content in elem.text:
             return True
-	return False
-	
+    return False
+
+
 def wait_for_elem(browser, xpath, timeout=15):
     start = time.time()
     elems = []
@@ -48,57 +50,65 @@ def wait_for_content(step, browser, content, timeout=15):
 @step('I am in homepage SELENE$')
 def homepage_selene(step):
     with AssertContextManager(step):
-        #world.browser.get('http://localhost:8080/#/?_k=ujlsa1')
+        # world.browser.get('http://localhost:8080/#/?_k=ujlsa1')
         world.browser.get('http://10.71.23.244/selene/#/?_k=63ld6h')
         world.browser.find_element_by_css_selector('.login-card.paper.white')
 
+
 @step('I press "(.*?)"$')
 def press(step, input_button_text):
-	with AssertContextManager(step):
-		button = world.browser.find_elelement_by_partial_link_text(input_button_text)
-		button.click()
+    with AssertContextManager(step):
+        button = world.browser.find_elelement_by_partial_link_text(input_button_text)
+        button.click()
         time.sleep(2)
+
 
 @step('I fill in the textfield with "(.*?)"$')
 def fill_in_textfield(step, entry):
-	with AssertContextManager(step):
-		textfield = world.browser.find_element_by_xpath("//input[@id='gs_htif0']")
-		textfield.clear()
-		textfield.send_keys(entry)
+    with AssertContextManager(step):
+        textfield = world.browser.find_element_by_xpath("//input[@id='gs_htif0']")
+        textfield.clear()
+        textfield.send_keys(entry)
+
 
 @step('I should see "(.*?)"$')
 def should_see(step, text, timeout=15):
-	with AssertContextManager(step):
-		start = time.time()
-		while time.time() - start < timeout:
-			if contains_content(world.browser, text):
-				return
-			time.sleep(2)
-		assert_true(step, contains_content(world.browser, text))
+    with AssertContextManager(step):
+        start = time.time()
+        while time.time() - start < timeout:
+            if contains_content(world.browser, text):
+                return
+            time.sleep(2)
+        assert_true(step, contains_content(world.browser, text))
         time.sleep(10)
-		
+
+
 @step('I press Ingresar$')
 def should_see(step):
-	with AssertContextManager(step):
-		button = world.browser.find_element_by_css_selector(".btn.btn-secondary.btn-block")
-		button.click()
-		
+    with AssertContextManager(step):
+        button = world.browser.find_element_by_css_selector(".btn.btn-secondary.btn-block")
+        button.click()
+
+
 @step('I am in Dashboar SELENE$')
 def dashboar_selene(step):
-	with AssertContextManager(step):
-		world.browser.find_element_by_css_selector('.fa.fa-plus-square-o.icon')
+    with AssertContextManager(step):
+        world.browser.find_element_by_css_selector('.fa.fa-plus-square-o.icon')
+
 
 @step('I press Registrar Dispositivo$')
 def press_Registrar_dispositivo(step):
-	with AssertContextManager(step):
-		button = world.browser.find_element_by_css_selector('section.sidenav i.fa.fa-plus-square.icon')
-		button.click()
-		
+    with AssertContextManager(step):
+        button = world.browser.find_element_by_css_selector('section.sidenav i.fa.fa-plus-square.icon')
+        button.click()
+
+
 @step('I am in "(.*?)" page$')
 def registrar_dispositivo_page(step, page):
-	with AssertContextManager(step):
-		page in world.browser.find_element_by_css_selector('.container-fluid>h1').text
+    with AssertContextManager(step):
+        page in world.browser.find_element_by_css_selector('.container-fluid>h1').text
         time.sleep(2)
+
 
 @step('I select Tipo like "(.*?)"$')
 def select_device_type(step, content):
@@ -106,11 +116,13 @@ def select_device_type(step, content):
         options = Select(world.browser.find_element_by_css_selector('select[name="device_type"]'))
         options.select_by_visible_text(content)
 
+
 @step('I select Marca like "(.*?)"$')
 def select_device_brand(step, content):
     with AssertContextManager(step):
         options = Select(world.browser.find_element_by_css_selector('select[name="device_brand"]'))
         options.select_by_visible_text(content)
+
 
 @step('I select Activo like "(.*?)"$')
 def select_device_activo(step, content):
@@ -120,12 +132,14 @@ def select_device_activo(step, content):
             if a.is_displayed() and content in a.text:
                 a.click()
 
+
 @step('I register Serial like "(.*?)"$')
 def register_device_serial(step, content):
     with AssertContextManager(step):
         world.browser.find_element_by_css_selector('input[name="serial_number"]').click()
         world.browser.find_element_by_css_selector('input[name="serial_number"]').clear()
         world.browser.find_element_by_css_selector('input[name="serial_number"]').send_keys(content)
+
 
 @step('I register Modelo like "(.*?)"$')
 def register_device_model(step, content):
@@ -134,6 +148,7 @@ def register_device_model(step, content):
         world.browser.find_element_by_css_selector('input[name="model"]').clear()
         world.browser.find_element_by_css_selector('input[name="model"]').send_keys(content)
 
+
 @step('I select Fecha de Compra like "(.*?)"$')
 def register_device_purchase_date(step, content):
     with AssertContextManager(step):
@@ -141,11 +156,13 @@ def register_device_purchase_date(step, content):
         world.browser.find_element_by_css_selector('input[name="purchase_date"]').clear()
         world.browser.find_element_by_css_selector('input[name="purchase_date"]').send_keys(content)
 
+
 @step('I select Propiedad like "(.*?)"$')
 def select_device_ownership(step, content):
     with AssertContextManager(step):
         options = Select(world.browser.find_element_by_css_selector('select[name="ownership"]'))
         options.select_by_visible_text(content)
+
 
 @step('I register Responsable like "(.*?)"$')
 def assignee_name(step, content):
@@ -154,11 +171,13 @@ def assignee_name(step, content):
         world.browser.find_element_by_css_selector('input[name="assignee_name"]').clear()
         world.browser.find_element_by_css_selector('input[name="assignee_name"]').send_keys(content)
 
+
 @step('I select Proyecto like "(.*?)"$')
 def assignee_project(step, content):
     with AssertContextManager(step):
         options = Select(world.browser.find_element_by_css_selector('select[name="project"]'))
         options.select_by_visible_text(content)
+
 
 @step('I register Fecha de Entrega')
 def assingee_expected_return_date(step):
@@ -168,17 +187,20 @@ def assingee_expected_return_date(step):
         day.click()
         time.sleep(2)
 
+
 @step('I select first device$')
 def assignee_first_device(step):
     with AssertContextManager(step):
         check = world.browser.find_element_by_css_selector('input[type="checkbox"]:first-child')
         check.click()
 
+
 @step('I select first device detail$')
 def assignee_first_device_detail(step):
     with AssertContextManager(step):
         check = world.browser.find_element_by_css_selector('tr.data-row i.fa.fa-search:first-child')
         check.click()
+
 
 @step('I press Guardar$')
 def save_device(step):
@@ -187,12 +209,14 @@ def save_device(step):
         button.click()
         time.sleep(2)
 
+
 @step('I press Aceptar$')
 def ok_detail(step):
     with AssertContextManager(step):
         button = world.browser.find_element_by_css_selector('a[href="#/assigned_device_list"] i')
         button.click()
         time.sleep(2)
+
 
 @step('I press Dashboard$')
 def press_dashboard(step):
@@ -201,6 +225,7 @@ def press_dashboard(step):
         button.click()
         time.sleep(2)
 
+
 @step('I press Asignar$')
 def asing_device_page(step):
     with AssertContextManager(step):
@@ -208,12 +233,14 @@ def asing_device_page(step):
         button.click()
         time.sleep(2)
 
+
 @step('I press Dispositivos')
 def asing_device_page(step):
     with AssertContextManager(step):
         button = world.browser.find_element_by_css_selector('i.fa.fa-desktop.icon')
         button.click()
         time.sleep(2)
+
 
 @step('I press button "(.*?)"$')
 def press_button(step, content):
--- backend/e2e/terrain.py
+++ backend/e2e/terrain.py
@@ -1,18 +1,17 @@
-from lettuce import *
+from lettuce import before, after, world
 import lettuce_webdriver.webdriver
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 @before.all
 def setup_browser():
-	#world.browser = webdriver.Firefox()
-	world.browser = webdriver.Chrome()
-	world.browser.implicitly_wait(1)
-	#world.browser.get('http://localhost:8080/#/?_k=ujlsa1')
-	#world.browser.get('http://10.71.23.244/selene/#/?_k=63ld6h')
-	#world.browser.maximize_window()
+    world.browser = webdriver.Firefox()
+    world.browser.implicitly_wait(1)
+    #  world.browser.get('http://localhost:8080/#/?_k=ujlsa1')
+    #  world.browser.get('http://10.71.23.244/selene/#/?_k=63ld6h')
+    #  world.browser.maximize_window()
+
 
 @after.all
 def tear_down_feature(feature):
-	world.browser.quit()
+    world.browser.quit()
