06bb5a8bab0b542e98af5ca80f3efc47b7134b45
faustocv <fausto.castaneda.v@gmail.com>
2016-10-05 16:26:48 -0500

    Adds tests for postgres provisioning

---
+++ vagrant/provision/spec/default/postgres_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe package('postgresql') do
+  it { should be_installed }
+end
+
+describe package('python-psycopg2') do
+  it { should be_installed }
+end
+
+describe package('acl') do
+  it { should be_installed }
+end
+
+describe port(5432) do
+  it { should be_listening }
+end
+
+describe file('/etc/postgresql/9.1/main/pg_hba.conf') do
+  it 'should disable peer connection for postgres user' do
+    should_not contain 'local   all             postgres                                peer'
+  end
+
+  it 'should enable md5 connection for postgres user' do
+    should contain 'local   all             postgres                                md5'
+  end
+end
+
+describe command('PGPASSWORD=postgres psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname=\'selene\'"') do
+  its (:stdout) { should match /1/ }
+end
+
+#describe command('psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname=\'postgres\'"') do
+  #its (:stderr) { should match /xxx/ }
+#end
+
+
